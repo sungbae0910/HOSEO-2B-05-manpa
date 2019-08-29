4b26c8fffa7aaf25d0db2502cc1319c8346bb647
sungabae <dkrnlwkr1@naver.com>
2019-08-29 13:37:09 +0900

    2019-08-29src

---
+++ com/manpa/post/postDto.java
@@ -0,0 +1,38 @@
+package com.manpa.post;
+
+import java.sql.Timestamp;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class loginDto
+ */
+public class postDto {
+	private String post;
+	private String juso;
+	
+	
+	
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+	public String getJuso() {
+		return juso;
+	}
+	public void setJuso(String juso) {
+		this.juso = juso;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+
+
